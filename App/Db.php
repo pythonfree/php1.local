0bb86276f954ca499aa398af290493a892bff256
pseex <evgeniyshakhov@gmail.com>
2021-05-05 23:07:57 +0300

    Добавили обработку исключения ошибки подключения к БД

--- App/Db.php
+++ App/Db.php
@@ -10,10 +10,14 @@ class Db
 
     public function __construct()
     {
-        $config = (include __DIR__ . '/config.php')['db'];
-        $this->dbh = new \PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'],
-        $config['user'], $config['password']
-        );
+        try {
+            $config = (include __DIR__ . '/config.php')['db'];
+            $this->dbh = new \PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'],
+                $config['user'], $config['password']
+            );
+        } catch (\PDOException  $error) {
+            throw new DbException('Ошибка подключения к базе данных.');
+        }
     }
 
     public function query($sql, $data=[], $class)
