a9bc3ec67d7c3b0719054722746adff0cb92aa26
pseex <evgeniyshakhov@gmail.com>
2021-05-06 15:01:25 +0300

    Изменили автозагрузку на современный стандарт

--- App/autoload.php
+++ App/autoload.php
@@ -1,6 +1,5 @@
 <?php
 
-function __autoload($class)
-{
+spl_autoload_register(function ($class) {
     require __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php';
-}
+});
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 include __DIR__ . '/../App/autoload.php';
 
 
